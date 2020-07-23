f8a4c47dceb048bea94f31ae5bac339455d6f71b
HCK <huang.chengkai1981@gmail.com>
2020-07-24 01:56:45 +0800

    set new SelectWeight

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const example = require('./prizenumber');
+const example = require('./power_lotto_number');
 const { ary, forInRight } = require('lodash');
 
 // setting
 const N_NUMBERS = 38;
-const SelectWeigh ={countIndex:[8,30,38], select:[2,2,2]};
+//const SelectWeight ={countIndex:[8, 20, 38], select:[2, 2, 2]};
+const SelectWeight ={countIndex:[4, 10, 19, 28, 34,  38], select:[1, 1,1,1,1,1]};
 
 // get opend number history
 const text = example.getText();
@@ -20,7 +21,7 @@ const startRange = (info) =>{
     return range;
 };
 
-const inputRange = [0, 60];
+const inputRange = [0, 30];
 const range = startRange(inputRange);
 
 // decide the weight of each number
@@ -28,7 +29,9 @@ const numberCount = new Array(N_NUMBERS+1).fill(1);
 numberCount[0] = 0;
 for (let index = range[0]; index < range[1]; index++) {
     for (let j = 2; j < text.number[index].length; j++) {
-        numberCount[text.number[index][j]] += (index - range[0] + 1);     
+      // numberCount[text.number[index][j]] += (1+ 0.14*( range[1]- index)); 
+       numberCount[text.number[index][j]] += (1);     
+    
     }
 }
 
@@ -51,30 +54,30 @@ sortNumber.sort(function(a, b) {
 delete sortNumber[sortNumber.length-1];
 
 // Sort by high and low
-let selectArea = new Array(SelectWeigh.countIndex.length);
-for (let index = 0; index < SelectWeigh.countIndex.length; index++) {
+let selectArea = new Array(SelectWeight.countIndex.length);
+for (let index = 0; index < SelectWeight.countIndex.length; index++) {
     selectArea[index] = [];
 }
 for (let index = 0; index < sortNumber.length; index++) {
-    for (let i = 0; i < SelectWeigh.countIndex.length; i++) {
-        if(index < SelectWeigh.countIndex[i]){
+    for (let i = 0; i < SelectWeight.countIndex.length; i++) {
+        if(index < SelectWeight.countIndex[i]){
             selectArea[i].push(sortNumber[index][1]);
             break;
         }        
     }
-    // if(index < SelectWeigh.countIndex[0]){
+    // if(index < SelectWeight.countIndex[0]){
     //     selectArea[0].push(sortNumber[index][1]);
     // }
-    // else if(index < SelectWeigh.countIndex[1]){
+    // else if(index < SelectWeight.countIndex[1]){
     //     selectArea[1].push(sortNumber[index][1]);
     // }
-    // else if(index < SelectWeigh.countIndex[2]){
+    // else if(index < SelectWeight.countIndex[2]){
     //     selectArea[2].push(sortNumber[index][1]);
     // }
-    // else if(index < SelectWeigh.countIndex[3]){
+    // else if(index < SelectWeight.countIndex[3]){
     //     selectArea[3].push(sortNumber[index][1]);
     // }
-    // else if(index < SelectWeigh.countIndex[4]){
+    // else if(index < SelectWeight.countIndex[4]){
     //     selectArea[4].push(sortNumber[index][1]);
     // }
 }
@@ -83,24 +86,39 @@ for (let index = 0; index < sortNumber.length; index++) {
 function shuffle(array) {
     array.sort(() => Math.random() - 0.5);
 }
-const randNumber = [];
-for (let index = 0; index < selectArea.length; index++) {
-    shuffle(selectArea[index]);
-}
-for (let index = 0; index < SelectWeigh.select.length; index++) {
-    for (let i = 0; i < SelectWeigh.select[index]; i++) {
-        randNumber.push(selectArea[index][i]);
+let time = 0;
+for (let count = 0; count < 8; count++) {
+
+    const randNumber = [];
+    for (let index = 0; index < selectArea.length; index++) {
+        shuffle(selectArea[index]);
     }
+    for (let index = 0; index < SelectWeight.select.length; index++) {
+        for (let i = 0; i < SelectWeight.select[index]; i++) {
+            randNumber.push(selectArea[index][i]);
+        }
+    }
+    
+    //
+    randNumber.sort(function(a, b) {
+        if (a === b) {
+            return 0;
+        }
+        else {
+            return (a< b) ? -1 : 1;
+        }  
+    });
+   console.log(randNumber);
+
+
+    // const bingoNumber = randNumber.filter(info=> info === text.number[inputRange[0]-1][2] || 
+    //     info === text.number[inputRange[0]-1][3]  || info === text.number[inputRange[0]-1][4]  || info === text.number[inputRange[0]-1][5]  
+    //     || info === text.number[inputRange[0]-1][6]  || info ===text.number[inputRange[0]-1][7]  );
+    // if(bingoNumber.length >=4){
+    //     time++;
+    //     console.log(`time: ${time}, ${randNumber}, bingoNumber: ${bingoNumber.length}, hitRate: ${time*100/(count+1)} %`);    
+
+    // }
 }
 
-//
-randNumber.sort(function(a, b) {
-    if (a === b) {
-        return 0;
-    }
-    else {
-        return (a< b) ? -1 : 1;
-    }  
-});
-console.log(randNumber);
 
